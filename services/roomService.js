32c7495a60dc5d3fc9ae896c2f93448df479ff68
guminki <rnalsrl93@gmail.com>
2022-01-21 02:43:02 +0900

    add : list filtering

--- services/roomService.js
+++ services/roomService.js
@@ -1,11 +1,16 @@
+<<<<<<< HEAD
 const { response } = require('express');
 const { roomDao } = require('../models');
+=======
+const roomDao = require('../models/roomDao');
+>>>>>>> 377c6a1 (add : list filtering)
 
 const getRoomList = async (
 	location,
 	checkin,
 	checkout,
 	person,
+<<<<<<< HEAD
 	roomTypeIdForSort,
 	optionIdForSort,
 	userId,
@@ -14,11 +19,17 @@ const getRoomList = async (
 ) => {
 	const region = await roomDao.getLocationLatLng(location);
 
+=======
+	roomTypeId,
+	convenienceIdForSort,
+) => {
+>>>>>>> 377c6a1 (add : list filtering)
 	const roomList = await roomDao.getRoomList(
 		location,
 		checkin,
 		checkout,
 		person,
+<<<<<<< HEAD
 		roomTypeIdForSort,
 		optionIdForSort,
 		userId,
@@ -38,3 +49,17 @@ const getOptions = async () => {
 };
 
 module.exports = { getRoomList, getOptions };
+=======
+		roomTypeId,
+		convenienceIdForSort,
+	);
+
+	return roomList;
+};
+
+const getConveniences = async () => {
+	return await roomDao.getConveniences();
+};
+
+module.exports = { getRoomList, getConveniences };
+>>>>>>> 377c6a1 (add : list filtering)
