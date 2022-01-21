71dc44d48f2ca6dcc79166fee8864fe06121d381
박효상 <45405912+pd0203@users.noreply.github.com>
2022-01-21 17:40:40 +0900

    Fix: token problem in kakao login API solved
    Fix: token problem solved
    
    Fix: token problem solved

--- utils/token.js
+++ utils/token.js
@@ -2,13 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const salt = 'OURBNBSALT';
 
-const loginToken = async (snsId) => {
-  return await jwt.sign({ id: snsId }, salt, { expiresIn: '30m' });
+const loginToken = (snsId) => {
+  return jwt.sign({ id: snsId }, salt, { expiresIn: '30m' });
 };
 
-const verifyToken = async (token) => {
+const verifyToken = (token) => {
   try {
-    return await jwt.verify(token, salt);
+    return jwt.verify(token, salt);
   } catch (err) {
     return null;
   }
