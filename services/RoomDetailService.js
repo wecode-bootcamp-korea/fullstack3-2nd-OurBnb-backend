0f9f409611471686e85a5ceaa8b74c5683ae5804
박효상 <45405912+pd0203@users.noreply.github.com>
2022-01-24 03:03:37 +0900

    Fix: detail API - test necessary

--- services/RoomDetailService.js
+++ services/RoomDetailService.js
@@ -2,9 +2,22 @@ const { roomDetailDao } = require('../models');
 
 const getRoomDetail = async (roomId) => {
 
-  const detail = await roomDetailDao.getRoomDetail(roomId);
+  const detail = {}; 
 
-  if (!detail) {
+  const mainInfo = await roomDetailDao.getMainInfo(roomId);
+  const option = await roomDetailDao.getOption(roomId); 
+  const benefit = await roomDetailDao.getBenefit(roomId); 
+  const rule = await roomDetailDao.getRule(roomId); 
+  const safety = await roomDetailDao.getSafety(roomId); 
+
+  detail['mainInfo'] = mainInfo; 
+  detail['option'] = option;
+  detail['benefit'] = benefit;
+  detail['rule'] = rule;
+  detail['safety'] = safety;  
+
+  if (!detail['mainInfo'] || !detail['option'] || !detail['benefit'] || 
+      !detail['rule'] || !detail['safety']) {
     const error = new Error('ROOM DETAIL LOAD FAILED');
     error.statusCode = 400;
     throw error;
