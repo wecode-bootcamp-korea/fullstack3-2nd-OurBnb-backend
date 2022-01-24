32c6d57150c266579049eadeece1eb71fd60a1ab
guminki <rnalsrl93@gmail.com>
2022-01-24 22:46:21 +0900

    modify : list api

--- models/roomDao.js
+++ models/roomDao.js
@@ -1,14 +1,6 @@
 const prisma = require('./index');
 
-const getRoomList = async (
-	location,
-	checkin,
-	checkout,
-	person,
-	roomTypeId,
-	optionIdForSort,
-	userId,
-) => {
+const getLocationLatLng = async location => {
 	const [region] = await prisma.$queryRaw`
     SELECT
       locations.name,
@@ -18,7 +10,18 @@ const getRoomList = async (
     WHERE
       locations.name LIKE CONCAT("%",${location},"%")
   `;
+	return region;
+};
 
+const getRoomList = async (
+	location,
+	checkin,
+	checkout,
+	person,
+	roomTypeId,
+	optionIdForSort,
+	userId,
+) => {
 	const roomList = await prisma.$queryRaw`
     SELECT
       rooms.id AS roomId,
@@ -50,16 +53,15 @@ const getRoomList = async (
     JOIN room_types ON room_types.id = rooms.room_type_id
     JOIN locations ON locations.id = rooms.location_id
     JOIN rooms_options ON rooms_options.room_id = rooms.id
-    JOIN options ON options.id = rooms_options.option_id 
-    JOIN reservations ON reservations.room_id = rooms.id
+    JOIN options ON options.id = rooms_options.option_id
     WHERE
     -- location이 포함된 컬럼만 선택
       locations.name LIKE CONCAT("%",${location},"%")
     AND
     -- 내가 선택한 체크인, 체크아웃 범위에 포함이 안된 컬럼만 선택
-      reservations.check_in NOT BETWEEN '2022-01-19' AND '2022-01-26'
+      rooms.id NOT IN (SELECT reservations.room_id FROM reservations WHERE reservations.check_in BETWEEN '2022-01-19' AND '2022-01-26')
     AND
-      reservations.check_out NOT BETWEEN '2022-01-19' AND '2022-01-26'
+      rooms.id NOT IN (SELECT reservations.room_id FROM reservations WHERE reservations.check_out BETWEEN '2022-01-19' AND '2022-01-26')
     AND
     -- 사람수가 있다면 선택한 사람수보다 수용범위가 큰 컬럼만 선택
       if(${person}, rooms.guest_capacity >= ${person} , rooms.guest_capacity is NOT NULL)
@@ -74,11 +76,9 @@ const getRoomList = async (
       if(${optionIdForSort}, options.id IN (${optionIdForSort}), options.id is NOT NULL)
     GROUP BY rooms.id
     ORDER BY rooms.id
-    LIMIT 1
-    OFFSET 0
   `;
 
-	return { region, roomList };
+	return roomList;
 };
 
 const getOptions = async () => {
@@ -91,4 +91,4 @@ const getOptions = async () => {
   `;
 };
 
-module.exports = { getRoomList, getOptions };
+module.exports = { getRoomList, getOptions, getLocationLatLng };
