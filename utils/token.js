fd2f0d889e38de3f3fecfa794e7ac4f45817c525
guminki <rnalsrl93@gmail.com>
2022-01-26 21:45:12 +0900

    미들들웨어 오류 수정

--- utils/token.js
+++ utils/token.js
@@ -2,16 +2,16 @@ const jwt = require('jsonwebtoken');
 
 const salt = 'OURBNBSALT';
 
-const loginToken = (id) => {
-  return jwt.sign({ id: id }, salt, { expiresIn: '30m' });
+const loginToken = id => {
+	return jwt.sign({ id: id }, salt, { expiresIn: '30m' });
 };
 
-const verifyToken = (token) => {
-  try {
-    return jwt.verify(token, salt);
-  } catch (err) {
-    return null;
-  }
+const verifyToken = token => {
+	try {
+		return jwt.verify(token, salt);
+	} catch (err) {
+		return null;
+	}
 };
 
 module.exports = { loginToken, verifyToken };
