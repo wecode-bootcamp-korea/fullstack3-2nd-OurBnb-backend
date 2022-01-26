6da8266a39e078db0f4d1d1e7d4fd083e73f4a0e
박효상 <45405912+pd0203@users.noreply.github.com>
2022-01-26 18:43:54 +0900

    Add: review get API

--- routes/roomRouter.js
+++ routes/roomRouter.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { roomController, roomDetailController, reservationController } = require('../controllers');
+const {
+	roomController,
+	roomDetailController,
+	roomReviewController,
+	reservationController,
+} = require('../controllers');
 const { checkToken } = require('../middleware/auth');
 
 router.get('/', checkToken, roomController.getRoomList);
