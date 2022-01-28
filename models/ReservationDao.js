91cfc75fd185157526cfb98124be3c73458183dd
박효상 <45405912+pd0203@users.noreply.github.com>
2022-01-28 12:45:19 +0900

    Fix: reservation API

--- models/ReservationDao.js
+++ models/ReservationDao.js
@@ -1,12 +1,42 @@
 const prisma = require('./index');
 
 const postReservation = async (guestCount, checkIn, checkOut, userId, roomId) => {
-    return await prisma.$queryRaw`
-      INSERT INTO
-        reservations (guest_count, check_in, check_out, user_id, room_id)
-      VALUES 
-        (${guestCount}, ${checkIn}, ${checkOut}, ${userId}, ${roomId}) 
-    `; 
-  }
-  
-  module.exports = { postReservation };
+  return await prisma.$queryRaw`
+    INSERT INTO
+      reservations (guest_count, check_in, check_out, user_id, room_id)
+    VALUES 
+      (${guestCount}, ${checkIn}, ${checkOut}, ${userId}, ${roomId}) 
+  `; 
+}
+
+const getReservationForCheck = async (roomId, checkIn, checkOut) => {
+  const [reservation] = await prisma.$queryRaw`
+    SELECT
+      reservations.room_id AS roomId,
+      users.id AS userId,
+      reservations.check_in AS checkIn,
+      reservations.check_out AS checkOut
+    FROM reservations
+    JOIN rooms ON rooms.id = reservations.room_id
+    JOIN users ON reservations.user_id = users.id
+    WHERE
+      ((DATE(${checkIn}) >= reservations.check_in AND DATE(${checkIn}) <= reservations.check_out) 
+      OR (DATE(${checkOut}) >= reservations.check_in AND DATE(${checkOut}) <= reservations.check_out)
+      OR (DATE(${checkIn}) <= reservations.check_in AND DATE(${checkOut}) >= reservations.check_out))
+    AND
+      reservations.room_id = ${roomId}
+  `;
+  return reservation; 
+}
+
+const getGuestMaxNumber = async (roomId) => {
+  const [total] = await prisma.$queryRaw`
+    SELECT
+      guest_capacity AS guestCapacity 
+    FROM rooms
+    WHERE
+      rooms.id = ${roomId}
+  `;
+  return total;
+}
+  module.exports = { postReservation, getReservationForCheck, getGuestMaxNumber };
--- utils/token.js
+++ utils/token.js
@@ -2,8 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const salt = 'OURBNBSALT';
 
+<<<<<<< HEAD
 const loginToken = id => {
 	return jwt.sign({ id: id }, salt, { expiresIn: '30m' });
+=======
+const loginToken = (id) => {
+  return jwt.sign({ id: id }, salt, { expiresIn: '2h' });
+>>>>>>> cfc4e1b (Fix: reservation API)
 };
 
 const verifyToken = token => {
