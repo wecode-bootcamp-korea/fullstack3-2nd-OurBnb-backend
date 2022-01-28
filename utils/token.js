53a657a9f674ee2ecd26204edaaf73c22e3ec7ac
박효상 <45405912+pd0203@users.noreply.github.com>
2022-01-28 13:28:24 +0900

    Fix: error

--- utils/token.js
+++ utils/token.js
@@ -2,13 +2,8 @@ const jwt = require('jsonwebtoken');
 
 const salt = 'OURBNBSALT';
 
-<<<<<<< HEAD
-const loginToken = id => {
-	return jwt.sign({ id: id }, salt, { expiresIn: '30m' });
-=======
 const loginToken = (id) => {
   return jwt.sign({ id: id }, salt, { expiresIn: '2h' });
->>>>>>> cfc4e1b (Fix: reservation API)
 };
 
 const verifyToken = token => {
