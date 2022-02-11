305e1c6aef097a4969bcd1bdc64e4738e439d044
박효상 <45405912+pd0203@users.noreply.github.com>
2022-02-11 15:21:52 +0900

    Fix: 멘토링 받은 대로 리팩토링 완료

--- services/RoomReviewService.js
+++ services/RoomReviewService.js
@@ -1,6 +1,5 @@
 const { roomReviewDao } = require('../models');
 
-<<<<<<< HEAD
 const getRoomReview = async roomId => {
 	const reviewInfo = {};
 
@@ -34,39 +33,3 @@ const getRoomReview = async roomId => {
 };
 
 module.exports = { getRoomReview };
-=======
-const getRoomReview = async (roomId) => {
-
-  const reviewInfo = {};   
-
-  const roomAvgRate = await roomReviewDao.getAvgRoomRate(roomId); 
-  const roomReviewCount = await roomReviewDao.getRoomReviewCount(roomId);
-  const roomReview = await roomReviewDao.getRoomReview(roomId); 
-
-  if (!roomAvgRate) {
-    const error = new Error('ROOM_AVERAGE_RATE_LOAD_FAILED');
-    error.statusCode = 400;
-    throw error;
-  }  
-  
-  if (!roomReviewCount) {
-    const error = new Error('ROOM_REVIEW_COUNT_LOAD_FAILED');
-    error.statusCode = 400;
-    throw error;
-  } 
-
-  if (!roomReview) {
-    const error = new Error('ROOM_REVIEW_LOAD_FAILED');
-    error.statusCode = 400;
-    throw error;
-  }  
-
-  reviewInfo['roomAvgRate'] = roomAvgRate;
-  reviewInfo['roomReviewCount'] = roomReviewCount;
-  reviewInfo['roomReview'] = roomReview;
-  
-  return reviewInfo;
-};
-
-module.exports = { getRoomReview };
->>>>>>> d230fc4 (Add: review get API)
